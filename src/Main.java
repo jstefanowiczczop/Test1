1db4e2bb7f7994eef99166a046020f6ef7b5020b
jstefanowiczczop <jstefanowicz.czop@gmail.com>
2016-10-24 21:40:17 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int x = 2;
-        if (x == 2) {
+        int x = 3;
+        if (x >2) {
 
             // write your code here
             System.out.println("Test!");
